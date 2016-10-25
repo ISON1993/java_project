079cb6318260dd64020f36a15a0c37704b3f8392
tuzhenyu <2250708448@qq.com>
2016-10-25 21:14:51 +0800

    toString_test

---
+++ java_test/src/toString_test/FlagClass.java
@@ -0,0 +1,15 @@
+package toString_test;
+
+/**
+ * Created by user on 2016/10/25.
+ */
+public class FlagClass {
+    private  String s;
+    public FlagClass(){
+        System.out.println("create a FlagClass");
+        s = "s is created";
+    }
+    public String toString(){
+        return s;
+    }
+}
