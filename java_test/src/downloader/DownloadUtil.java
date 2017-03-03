715ef8252a9b3a5fcd9d6822c2b78725d8e83a87
zhenyutu <1572629866@qq.com>
2017-03-03 20:24:33 +0800

    定义连接

--- java_test/src/downloader/DownloadUtil.java
+++ java_test/src/downloader/DownloadUtil.java
@@ -1,8 +1,19 @@
 package downloader;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by tuzhenyu on 17-3-3.
  * @author tuzhenyu
  */
 public class DownloadUtil {
+    public HttpURLConnection getConnection(String path) throws Exception{
+        URL url = new URL(path);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setConnectTimeout(5*1000);
+
+        return conn;
+    }
 }
