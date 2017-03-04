bf0ab3d684bf005aa2978e5b383fdbf3a46fd71e
zhenyutu <1572629866@qq.com>
2017-03-04 08:46:08 +0800

    完成单线程图片抓取

---
+++ java_test/src/internet/Downloader.java
@@ -0,0 +1,50 @@
+package internet;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by tuzhenyu on 17-3-4.
+ * @author tuzhenyu
+ */
+public class Downloader {
+    public static void main(String[] args) {
+        download();
+    }
+
+    private static void download(){
+        URL url = null;
+        HttpURLConnection conn = null;
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        File file = new File("/home/tuzhenyu/tmp/test.gif");
+        try{
+            url = new URL("https://www.baidu.com/img/baidu_sylogo1.gif");
+            conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5 * 1000);
+            bis = new BufferedInputStream(conn.getInputStream());
+            bos = new BufferedOutputStream(new FileOutputStream(file));
+            int len = 2048;
+            byte[] b = new byte[len];
+            while ((len = bis.read(b)) != -1)
+            {
+                bos.write(b, 0, len);
+            }
+            System.out.println("上传成功");
+            bos.flush();
+            bis.close();
+            conn.disconnect();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                bis.close();
+                bos.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
