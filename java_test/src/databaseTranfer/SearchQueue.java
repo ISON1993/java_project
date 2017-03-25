65042f46732bd273818299f501e2b83ff7189277
zhenyutu <1572629866@qq.com>
2017-03-25 19:06:57 +0800

    finish the table Program tranfer

--- java_test/src/databaseTranfer/SearchQueue.java
+++ java_test/src/databaseTranfer/SearchQueue.java
@@ -44,21 +44,29 @@ public class SearchQueue {
 //            String selectSql = "SELECT \"ID\",\"ResultDateTime\",\"ProgramID\",\"UnitID\",\"PositionID\" FROM \"Result\"" +
 //                    "WHERE \"ResultDateTime\" BETWEEN  to_date('2016-08-01 00:00:00','yyyy-MM-dd HH24:mi:ss')" +
 //                    "AND to_date('2016-09-01 23:00:00','yyyy-MM-dd HH24:mi:ss') AND \"UnitID\"="+tableName;
-            String selectSql = "SELECT r.ID,rt.\"FinalAngle\",rt.\"FinalTorque\" FROM \"Result\" r LEFT JOIN \"ResultTightening\" rt ON r.ID = rt.\"ResultID\"\n" +
-                    "WHERE \"ResultDateTime\" BETWEEN  to_date('2016-08-01 00:00:00','yyyy-MM-dd HH24:mi:ss')\n" +
-                    "AND to_date('2016-09-01 23:00:00','yyyy-MM-dd HH24:mi:ss') AND \"UnitID\"="+tableName;
+//            String selectSql = "SELECT r.ID,rt.\"FinalAngle\",rt.\"FinalTorque\" FROM \"Result\" r LEFT JOIN \"ResultTightening\" rt ON r.ID = rt.\"ResultID\"\n" +
+//                    "WHERE \"ResultDateTime\" BETWEEN  to_date('2016-08-01 00:00:00','yyyy-MM-dd HH24:mi:ss')\n" +
+//                    "AND to_date('2016-09-01 23:00:00','yyyy-MM-dd HH24:mi:ss') AND \"UnitID\"="+tableName;
+            String selectSql ="SELECT DISTINCT p.ID,p.\"Name\",a.\"ItemValue\",b.\"ItemValue\" FROM \"Result\" r " +
+                    "LEFT JOIN \"Program\" p ON r.\"ProgramID\" = p.ID " +
+                    "LEFT JOIN (SELECT \"ResultID\",\"ItemValue\" FROM \"ResultValueFloat\" rvf WHERE rvf.\"ResultValueTypeID\"=73) a ON r.ID = a.\"ResultID\"" +
+                    "LEFT JOIN (SELECT \"ResultID\",\"ItemValue\" FROM \"ResultValueFloat\" rvf WHERE rvf.\"ResultValueTypeID\"=74) b ON r.ID = b.\"ResultID\"" +
+                    "WHERE r.\"ResultDateTime\" BETWEEN  to_date('2016-08-01 00:00:00','yyyy-MM-dd HH24:mi:ss')" +
+                    "AND to_date('2016-09-01 23:00:00','yyyy-MM-dd HH24:mi:ss') AND \"UnitID\"=" +tableName;
+
             Connection oracleConn = OracleConnectionManager.getInstance().getConnection();
             Connection mysqlConn = MysqlConnectionManager.getInstance().getConnection();
             try {
                 mysqlConn.setAutoCommit(false);
-                PreparedStatement mysqlPstmt = mysqlConn.prepareStatement("INSERT INTO ResultTightening VALUES (?,?,?)");
+                PreparedStatement mysqlPstmt = mysqlConn.prepareStatement("INSERT INTO Program VALUES (?,?,?,?)");
                 Statement sm = oracleConn.createStatement();
                 ResultSet rs = sm.executeQuery(selectSql);
                 int batchCounter = 0;
                 while (rs.next()) {
                     mysqlPstmt.setLong(1, rs.getLong(1));
-                    mysqlPstmt.setFloat(2, rs.getFloat(2));
+                    mysqlPstmt.setNCharacterStream(2, rs.getNCharacterStream(2));
                     mysqlPstmt.setFloat(3, rs.getFloat(3));
+                    mysqlPstmt.setFloat(4, rs.getFloat(4));
 
                     mysqlPstmt.addBatch();
                     batchCounter++;
