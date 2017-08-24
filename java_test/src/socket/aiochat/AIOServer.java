ed919fc97feba333428c6fc8bde1b5835e16091d
zhenyutu <1572629866@qq.com>
2017-08-24 16:58:34 +0800

    修改异步聊天室

--- java_test/src/socket/aiochat/AIOServer.java
+++ java_test/src/socket/aiochat/AIOServer.java
@@ -30,6 +30,7 @@ public class AIOServer {
     public static void main(String[] args) throws Exception{
         AIOServer server = new AIOServer();
         server.startListen();
+        while (true){}
     }
 }
 
