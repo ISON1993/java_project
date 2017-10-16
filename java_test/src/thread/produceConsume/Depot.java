d9345820fcde7200454a215399b6997d3eb5d9d8
zhenyutu <1572629866@qq.com>
2017-10-16 15:40:08 +0800

    实现生产者消费者模型的lock和condition实现

--- java_test/src/thread/produceConsume/Depot.java
+++ java_test/src/thread/produceConsume/Depot.java
@@ -1,6 +1,10 @@
 package thread.produceConsume;
 
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by tuzhenyu on 17-10-16.
  * @author tuzhenyu
@@ -9,33 +13,43 @@ public class Depot {
     private int capacity;
     private int size;
 
+    private final Lock lock = new ReentrantLock();
+    private final Condition notEmpty = lock.newCondition();
+    private final Condition notFull = lock.newCondition();
+
     public Depot(int capacity){
         this.capacity = capacity;
         size = 0;
     }
 
-    public synchronized void produce(){
+    public void produce(){
+        lock.lock();
         try {
             while (size>=capacity)
-                wait();
+                notFull.await();
             size++;
             System.out.println("生产者生产一个，当前数量为："+size);
-            notify();
+            notEmpty.signal();
         }catch (Exception e){
             e.printStackTrace();
+        }finally {
+            lock.unlock();
         }
 
     }
 
-    public synchronized void consume(){
+    public void consume(){
+        lock.lock();
         try {
             while (size<=0)
-                wait();
+                notEmpty.await();
             size--;
             System.out.println("消费者消费一个，当前数量为："+size);
-            notify();
+            notFull.signal();
         }catch (Exception e){
             e.printStackTrace();
+        }finally {
+            lock.unlock();
         }
 
     }
